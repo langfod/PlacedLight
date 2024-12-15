56a996f0317d20a8c2c5e0de9d3ef03d0404abcd
FlayaN <hannes.feldt@gmail.com>
2024-12-15 20:43:44 +0100

    1.2

--- serialize.bat
+++ serialize.bat
@@ -2,3 +2,6 @@ SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light.esp" -o "Spriggi
 SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light Exteriors.esp" -o "Spriggit/Placed Light Exteriors" -g SkyrimSE -p Spriggit.Json
 SpriggitCLI\Spriggit.CLI.exe serialize -i "package/PL - Dark.esp" -o "Spriggit/PL - Dark" -g SkyrimSE -p Spriggit.Json
 SpriggitCLI\Spriggit.CLI.exe serialize -i "package/PL - Darkest.esp" -o "Spriggit/PL - Darkest" -g SkyrimSE -p Spriggit.Json
+SpriggitCLI\Spriggit.CLI.exe serialize -i "package/NOTWL - Lanterns.esp" -o "Spriggit/NOTWL - Lanterns" -g SkyrimSE -p Spriggit.Json
+SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - Vanilla Fixes.esp" -o "Spriggit/Placed Light - Vanilla Fixes" -g SkyrimSE -p Spriggit.Json
+SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light Exteriors - NOTWL 3 patch.esp" -o "Spriggit/Placed Light Exteriors - NOTWL 3 patch" -g SkyrimSE -p Spriggit.Json
