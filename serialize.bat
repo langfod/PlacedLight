b710a54a84028ca3cdd06705a48658c8948e754c
FlayaN <hannes.feldt@gmail.com>
2024-12-15 21:11:50 +0100

    1.2.4

--- serialize.bat
+++ serialize.bat
@@ -1,5 +1,9 @@
 SpriggitCLI\Spriggit.CLI.exe serialize -i "package/PL - Dark.esp" -o "Spriggit/PL - Dark" -g SkyrimSE -p Spriggit.Json
 SpriggitCLI\Spriggit.CLI.exe serialize -i "package/PL - Darkest.esp" -o "Spriggit/PL - Darkest" -g SkyrimSE -p Spriggit.Json
+SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light -  JK's Dark Brotherhood Sanctuary.esp" -o "Spriggit/Placed Light -  JK's Dark Brotherhood Sanctuary" -g SkyrimSE -p Spriggit.Json
+SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light -  JK's Jorrvaskr.esp" -o "Spriggit/Placed Light -  JK's Jorrvaskr" -g SkyrimSE -p Spriggit.Json
+SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light -  JK's Nightingale Hall.esp" -o "Spriggit/Placed Light -  JK's Nightingale Hall" -g SkyrimSE -p Spriggit.Json
+SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light -  JK's Septimus Signus's Outpost.esp" -o "Spriggit/Placed Light -  JK's Septimus Signus's Outpost" -g SkyrimSE -p Spriggit.Json
 SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - Arcadia's Cauldron.esp" -o "Spriggit/Placed Light - Arcadia's Cauldron" -g SkyrimSE -p Spriggit.Json
 SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - Castle Volkihar Rebuilt - Patch.esp" -o "Spriggit/Placed Light - Castle Volkihar Rebuilt - Patch" -g SkyrimSE -p Spriggit.Json
 SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - COTN Dawnstar - Patch.esp" -o "Spriggit/Placed Light - COTN Dawnstar - Patch" -g SkyrimSE -p Spriggit.Json
@@ -8,9 +12,13 @@ SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - JK's Bee and B
 SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - Jk's Belethor's General Goods.esp" -o "Spriggit/Placed Light - Jk's Belethor's General Goods" -g SkyrimSE -p Spriggit.Json
 SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - JK's Bits and Pieces.esp" -o "Spriggit/Placed Light - JK's Bits and Pieces" -g SkyrimSE -p Spriggit.Json
 SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - Jk's Candlehearth Hall.esp" -o "Spriggit/Placed Light - Jk's Candlehearth Hall" -g SkyrimSE -p Spriggit.Json
+SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - JK's Castle Dour.esp" -o "Spriggit/Placed Light - JK's Castle Dour" -g SkyrimSE -p Spriggit.Json
+SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - JK's Castle Volkihar.esp" -o "Spriggit/Placed Light - JK's Castle Volkihar" -g SkyrimSE -p Spriggit.Json
 SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - Jk's Dragonsreach.esp" -o "Spriggit/Placed Light - Jk's Dragonsreach" -g SkyrimSE -p Spriggit.Json
 SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - JK's Elgrims Elixirs.esp" -o "Spriggit/Placed Light - JK's Elgrims Elixirs" -g SkyrimSE -p Spriggit.Json
+SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - JK's Fort Dawnguard.esp" -o "Spriggit/Placed Light - JK's Fort Dawnguard" -g SkyrimSE -p Spriggit.Json
 SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - JK's Haelga's Bunkhouse.esp" -o "Spriggit/Placed Light - JK's Haelga's Bunkhouse" -g SkyrimSE -p Spriggit.Json
+SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - JK's High Hrothgar.esp" -o "Spriggit/Placed Light - JK's High Hrothgar" -g SkyrimSE -p Spriggit.Json
 SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - JK's Mistveil Keep.esp" -o "Spriggit/Placed Light - JK's Mistveil Keep" -g SkyrimSE -p Spriggit.Json
 SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - JK's New Gnisis Cornerclub.esp" -o "Spriggit/Placed Light - JK's New Gnisis Cornerclub" -g SkyrimSE -p Spriggit.Json
 SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - JK's Palace of the Kings.esp" -o "Spriggit/Placed Light - JK's Palace of the Kings" -g SkyrimSE -p Spriggit.Json
@@ -18,6 +26,7 @@ SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - JK's Radiant R
 SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - JK's Riverwood Trader.esp" -o "Spriggit/Placed Light - JK's Riverwood Trader" -g SkyrimSE -p Spriggit.Json
 SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - JK's Sadris Used Wares.esp" -o "Spriggit/Placed Light - JK's Sadris Used Wares" -g SkyrimSE -p Spriggit.Json
 SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - JK's Silver-Blood Inn.esp" -o "Spriggit/Placed Light - JK's Silver-Blood Inn" -g SkyrimSE -p Spriggit.Json
+SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - JK's Sky Haven Temple.esp" -o "Spriggit/Placed Light - JK's Sky Haven Temple" -g SkyrimSE -p Spriggit.Json
 SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - JK's Sleeping Giant Inn.esp" -o "Spriggit/Placed Light - JK's Sleeping Giant Inn" -g SkyrimSE -p Spriggit.Json
 SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - JK's Temple of Dibella.esp" -o "Spriggit/Placed Light - JK's Temple of Dibella" -g SkyrimSE -p Spriggit.Json
 SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - JK's Temple of Kynareth.esp" -o "Spriggit/Placed Light - JK's Temple of Kynareth" -g SkyrimSE -p Spriggit.Json
@@ -25,10 +34,12 @@ SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - JK's Temple of
 SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - JK's Temple of Talos.esp" -o "Spriggit/Placed Light - JK's Temple of Talos" -g SkyrimSE -p Spriggit.Json
 SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - JK's Temple of the Divines.esp" -o "Spriggit/Placed Light - JK's Temple of the Divines" -g SkyrimSE -p Spriggit.Json
 SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - JK's The Bannered Mare.esp" -o "Spriggit/Placed Light - JK's The Bannered Mare" -g SkyrimSE -p Spriggit.Json
+SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - JK's The Bards College.esp" -o "Spriggit/Placed Light - JK's The Bards College" -g SkyrimSE -p Spriggit.Json
 SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - JK's The Drunken Huntsman.esp" -o "Spriggit/Placed Light - JK's The Drunken Huntsman" -g SkyrimSE -p Spriggit.Json
 SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - JK's The Pawned Prawn.esp" -o "Spriggit/Placed Light - JK's The Pawned Prawn" -g SkyrimSE -p Spriggit.Json
 SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - JK's The Ragged Flagon.esp" -o "Spriggit/Placed Light - JK's The Ragged Flagon" -g SkyrimSE -p Spriggit.Json
 SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - JK's The Winking Skeever.esp" -o "Spriggit/Placed Light - JK's The Winking Skeever" -g SkyrimSE -p Spriggit.Json
+SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - JK's Thieves Guild.esp" -o "Spriggit/Placed Light - JK's Thieves Guild" -g SkyrimSE -p Spriggit.Json
 SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - JK's Understone Keep.esp" -o "Spriggit/Placed Light - JK's Understone Keep" -g SkyrimSE -p Spriggit.Json
 SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - JK's Warmaiden's.esp" -o "Spriggit/Placed Light - JK's Warmaiden's" -g SkyrimSE -p Spriggit.Json
 SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light - JK's White Phial.esp" -o "Spriggit/Placed Light - JK's White Phial" -g SkyrimSE -p Spriggit.Json
