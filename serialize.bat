9bb34ac61eed4625c8d734190b2bb44f0b794d32
FlayaN <hannes.feldt@gmail.com>
2024-12-15 20:41:40 +0100

    1.1

--- serialize.bat
+++ serialize.bat
@@ -1,2 +1,4 @@
 SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light.esp" -o "Spriggit/Placed Light" -g SkyrimSE -p Spriggit.Json
 SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light Exteriors.esp" -o "Spriggit/Placed Light Exteriors" -g SkyrimSE -p Spriggit.Json
+SpriggitCLI\Spriggit.CLI.exe serialize -i "package/PL - Dark.esp" -o "Spriggit/PL - Dark" -g SkyrimSE -p Spriggit.Json
+SpriggitCLI\Spriggit.CLI.exe serialize -i "package/PL - Darkest.esp" -o "Spriggit/PL - Darkest" -g SkyrimSE -p Spriggit.Json
