8f235f1cbcc2c83528de2453522f6b868606cc6b
FlayaN <hannes.feldt@gmail.com>
2024-12-15 20:40:11 +0100

    1.0

---
+++ serialize.bat
@@ -0,0 +1,2 @@
+SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light.esp" -o "Spriggit/Placed Light" -g SkyrimSE -p Spriggit.Json
+SpriggitCLI\Spriggit.CLI.exe serialize -i "package/Placed Light Exteriors.esp" -o "Spriggit/Placed Light Exteriors" -g SkyrimSE -p Spriggit.Json
